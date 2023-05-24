bf82f38f9f92652a2afcaf9273da375d04fa2f8c
khushi Patel <khushirp789@gmail.com>
2023-05-24 21:18:01 +0930

    Files for last prac

--- Document.h
+++ Document.h
@@ -2,9 +2,9 @@
 #define DOCUMENT_H
 #include<iostream>
 #include<string>
-
+//class document with private attributes
 class Document{
-friend class DocumentManager;
+friend class DocumentManager;  //friend class Manager 
 private:
 std::string name;
 int copies;
--- DocumentManager.h
+++ DocumentManager.h
@@ -6,10 +6,17 @@
 #include<vector>
 #include<unordered_map>
 
+//class Document Manager
 class DocumentManager{
 private:
-std::unordered_map<int, Document> DocumentsHeap; 
-std::vector<int> PatronVec; 
+std::unordered_map<int, Document> DocumentsHeap; //creating a heap for documents can pair int and documets
+                                                //+ can store in any order
+                                                //+ O(1) lookup , O(1) insertion [average]
+                                                //good choice as lots of data is expected
+std::vector<int> PatronVec; //creating a vector that stores integers
+                            //only need one thing to store so no need to pair
+                            //easy access O(1)
+                            //insertion is O(n) for worst case
 public:
 void addDocument(std::string name, int id, int license_limit);
 void addPatron(int patronID);
